1607e5d82b201b1360f643ce76d04200cd9a205e
ayiiissss10 <ayiiissss10@gmail.com>
2026-03-09 14:20:55 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -23,7 +23,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate("|#countdown 3||Hai Sayangku!|Semangat Terus|Kegiatannya Ya!|Have|A|Nice|Day!#rectangle|");
+      S.UI.simulate("|#countdown 3||Hai Sayangku!|Semangat Kegiatannya Ya!|Have|A|Nice|Day!||#rectangle|");
     }
 
     // Add a flag to track animation completion
